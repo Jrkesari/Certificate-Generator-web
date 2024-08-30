2ef514f4fae37c1edb4528da344bdbcc0b3d6457
Jrkesari <jrkesari@gmail.com>
2024-08-30 23:41:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,3 +71,5 @@ def upload_files():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Get the PORT environment variable, default to 5000
+    app.run(host='0.0.0.0', port=port)
